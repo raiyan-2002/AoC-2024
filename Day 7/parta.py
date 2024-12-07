78274570129a4616920b82b799606b8218df7f86
RAIYAN KHAN <raiyankhan2002@gmail.com>
2024-12-07 13:21:20 -0800

    day 7

---
+++ Day 7/parta.py
@@ -0,0 +1,41 @@
+filePath = "input.txt"
+
+res = []
+curr = []
+def generate(i, length):
+    if i == length:
+        res.append(curr[:]) 
+        return 
+    curr.append(0)
+    generate(i + 1, length)
+    curr.pop()
+    curr.append(1)
+    generate(i + 1, length)
+    curr.pop()
+
+ret = 0
+
+with open(filePath, 'r') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        target, nums = line.split(':')
+        target = int(target)
+        nums = nums.strip().split(' ')
+        nums = [int(num) for num in nums]
+        generate(0, len(nums) - 1)
+        ops = [[0] + op for op in res]
+        res = []
+
+        for op in ops:
+            start = 0
+            for i in range(len(op)):
+                if op[i] == 0:
+                    start += nums[i]
+                else:
+                    start *= nums[i]
+            
+            if start == target:
+                ret += target
+                break
+
+print(ret)
